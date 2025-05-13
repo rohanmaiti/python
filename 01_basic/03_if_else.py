b142fd432724a077cdc817f277206f76a5d4e839
rohanmaiti <maitirohan2019@gmail.com>
2025-05-13 22:06:14 +0530

    if elif else, nested

---
+++ 01_basic/03_if_else.py
@@ -0,0 +1,48 @@
+# Conditional Statement
+# focus on the syntax
+# age = 22
+# if(age >= 18 and age<21):
+#     print("can drive"); #indentaion (proper spacing)
+#     print("can vote");
+# elif(age>=21 and age<=35):
+#     print("Eigible for marraige");
+# else:
+#     print("default");
+
+# print("end of code");
+
+# same if like c,c++,java : if one statements runs then will not run other
+
+# nesting if else
+# age = 21
+# if(age >= 18):
+#     print("Above 18");
+#     if(age < 25):
+#         print("Under 25");
+#     else:
+#         print("Above 25");
+# else:
+#     print("Under 18");
+
+# Q. Check weather a number is odd or even
+# num = int(input("Enter a number: "));
+# if(num%2 == 0):
+#     print("Even number");
+# else:
+#     print("Odd number");
+
+# Q. Find Gretest of Three number
+# num1 = int(input("Enter num1: "));
+# num2 = int(input("Enter num2: "));
+# num3 = int(input("Enter num3: "));
+
+# if(num1 > num2):
+#     if(num1>num3):
+#         print(num1);
+#     else:
+#         print(num3);
+# elif(num2>num3):
+#     print(num2);
+# else:
+#     print(num3);
+
