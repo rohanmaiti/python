b6236f32207746e7c1999a234df42fb72dc50642
rohanmaiti <maitirohan2019@gmail.com>
2025-05-17 13:03:47 +0530

    Learned print, copying, datatypes, variable, operators, type casting, taking input

--- 01_basic/01_first.py
+++ 01_basic/01_first.py
@@ -15,20 +15,25 @@
 # price = 15.23;
 # print(name, age, price);
 # print("my name is :",name);
-name
 
-# age2 = age; # copying the value
+# #* can not have a uninitilised variable like this
+# fullname;  #* ERROR as not initialised
+# #* make it initialise with None
+# fullname = None;  #* in this case not error
+
+# age2 = age; #* copying the value
 # print(age2);
 # age = 23;
 # print(age);
 # print(age2);
 
+# *checking type
 # print(type(age));
 # print(type(price));
 # print(type(name));
 
-# Datatype
-# Integer, String (can be writen inside "str",'str','''str'''), Float, Boolean(True, Flase : note T and F are capital), None (None)
+# #* Datatype in python
+# #* Integer, String (can be writen inside "str",'str','''str'''), Float, Boolean(True, Flase : note T and F are capital), None (None)
 # x = True;
 # a = None;
 # print(type(x));
@@ -42,36 +47,36 @@ name
 multiline comment 
 """
 
-# Operator
-# Arithmetic Operator
+# #* Operator
+# #* Arithmetic Operator
 # a = 5
 # b = 2
 # print(a+b);
 # print(a-b);
 # print(a*b);
-# print(a/b); # will give float value
+# print(a/b); #* will give float value
 # print(a%b);
 # print(a**b);
 
-# Relational Operator
-# print(a == b);
-# print(a != b);
-
-# logical operator 
+# #* logical operator (or, and, not)
 # print(not False);
 # print(not (a>b));
 
 # print(True and False)
-# print((1 and 5));
+# print((0 and 5)); #* if any number is 0 and the operator is and them it returns 0 else returns the right side's number
+# print(5 and 3); #* will return 3;
+# print((5 or 0));
+# print(4 or 3);  #* if both the numbers are not non-zero and the operator is or then retuns the left side's number
+# print( 5 and 4 and 6 and 2);  #* will return the left most
 # print((a == b) or (b > a));
 
-# Type conversion
-# 1. Type conversion (automatic)
-# 2. Type casting (manully)
+# *Type conversion
+# *1. Type conversion (automatic)
+# *2. Type casting (manully)  --> SYNTAX: DataType(ele)
 
 # a = "1.2";
 # b = 5;
-# sum = a + b; # error
+# sum = a + b; #* error cause can only concatinate string unlike js where string + anything = string 
 # print(sum);
 
 # a = 1;
@@ -87,23 +92,23 @@ multiline comment
 # print(str(a));
 # print(type(str(a)))
 
-# taking input
-# name = input("enter your name : "); # take space also
+# #* taking input
+# name = input("enter your name : "); #* take space also and all input are by default string
 # print("your name is :",name);
 # print(type(name)); # all input value is bydefault string
 
-# taking int as a input 
+# #* taking int as a input 
 # num1 = int(input("Enter 1st number : "));
 # num2 = int(input("Enter 2nd numner : "));
 # print("Sum of two number is ", num1+num2);
 
-# safe side to take input as number
+# #* safe side to take input as number
 # num1 = float(input("Enter float num1 : "));
 # num2 = float(input("Enter float num2 : "));
 # print("Sum of two number is ", num1+num2);
 
 
-# question 
+# #* question 
 # take a input of a square and o/p the area
 # side = float(input("enter side of a square: "));
 # print("Area of square is ,",(side**2));
