e2d10280f899af59d3b07189e456005ad842cad9
rohanmaiti <maitirohan2019@gmail.com>
2025-07-14 22:05:02 +0530

    updated: class in python

--- 06_oops/01_class.py
+++ 06_oops/01_class.py
@@ -23,17 +23,47 @@ inova.color = 'red'
 # print(inova.color, inova.noOfWheel)
 
 # ****************************************************
-class Employee:
-    name='Rohan'
-    salary=120000
-    def getInfo(self):
-        print(f'Employee name {self.name} and salary {self.salary}')
+# class Employee:
+#     name='Rohan'
+#     salary=120000
+#     def getInfo(self):
+#         print(f'Employee name {self.name} and salary {self.salary}')
 
-emp1 = Employee()
-emp1.getInfo()
+# emp1 = Employee()
+# emp1.getInfo()
 # the above line is being called like this --> Employee.getInfo(emp1); 
 # that's why need to accept a argument at the function defination
 # every function in the function defination in a class need to write one argument "MUST"
+
+# ***************************
+#* Instance vs Class Variables
+# class Counter:
+#     count = 0   # class variable
+#     def __init__(self):
+#         Counter.count += 1  # modifies class variable
+#         self.id = Counter.count  # instance variable
+
+
+# ****************************
+#*  Instance / Class / Static Methods
+class Demo:
+    def instance_method(self):
+        print("Called instance method", self)
+
+    @classmethod
+    def class_method(cls):
+        print("Called class method", cls)
+
+    @staticmethod
+    def static_method():
+        print("Called static method")
+
+d = Demo()
+d.instance_method()
+Demo.class_method()
+Demo.static_method()
+
+
 
 
 
