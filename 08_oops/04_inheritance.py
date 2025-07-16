0af67adb697535ff09c7c972497efe1c72f7ee6d
rohanmaiti <maitirohan2019@gmail.com>
2025-07-16 23:20:09 +0530

    learn: oops updated

---
+++ 08_oops/04_inheritance.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def printCar(self):
+        print(f'Car brand = {self.brand} and modle = {self.model}')
+    
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, batttery_size):
+        super().__init__(brand, model)
+        self.batttery_size = batttery_size
+
+my_car = ElectricCar('Toyota', 'Fortuner', '1000kw')
+my_car.printCar()
