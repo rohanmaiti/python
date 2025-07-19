ede1e79e93f7726c6b6a186c15b1b19560dec1cf
rohanmaiti <maitirohan2019@gmail.com>
2025-07-19 15:50:54 +0530

    learn: api handling in python

---
+++ 11_api_handling/01.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_products():
+    try:
+        # comment: 
+         response = requests.get('https://dummyjson.com/products')
+         print(response)
+         print(response.json())
+    except Exception as e:
+        raise e
+   
+   
+get_products()
