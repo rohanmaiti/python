be2d5a844bc3beb761d9322e7053cddc0691741d
rohanmaiti <maitirohan2019@gmail.com>
2026-01-28 16:33:05 +0530

    learned: __name__

---
+++ 15_learning__/main.py
@@ -0,0 +1,3 @@
+# every file has a build in method that is __name__
+print(__name__)
+
---
+++ 15_learning__/xyz_route.py
@@ -0,0 +1,2 @@
+# if you directly run this file like python xyz_route.py then aslo __name__ will print __main__
+print(__name__)
